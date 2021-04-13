9cd4058f8681506dc5651b666ccb113dc9fd8bf2
Ryan Pascoe <ryan.pascoe@outlook.com>
2021-04-13 08:58:58 +0100

    Add sourcemaps to output, add umd version

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,32 @@ import pkg from './package.json';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
+let output = [
+	{
+		format: 'es',
+		file: pkg.module,
+		sourcemap: true,
+	},
+
+	{
+		format: 'cjs',
+		file: pkg.main,
+		sourcemap: true,
+	},
+
+	{
+		format: 'umd',
+		file: pkg.unpkg,
+		name: 'accordion',
+		sourcemap: true,
+		plugins: [ terser() ],
+	},
+];
+
 export default {
 	input: 'src/index.ts',
-	output: [
-		{
-			format: 'es',
-			file: pkg.module,
-		},
-
-		{
-			format: 'cjs',
-			file: pkg.main,
-		},
-	],
+	output: output,
 	plugins: [
-		terser(),
 		typescript({
 			declaration: false,
 			declarationDir: null,
