985d1a26731dea7a7bbd746f5fcac7fe279e66b6
Ryan Pascoe <ryan.pascoe@outlook.com>
2021-04-18 09:51:41 +0100

    Split out animation code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { defaults } from './defaults';
-import { getInnerHeight, modulo } from './utils';
+import { modulo } from './utils';
 import {
 	Accordion,
 	AccordionItem,
@@ -10,6 +10,7 @@ import {
 	Options,
 	Target,
 } from './types';
+import { up, down } from './animate';
 
 export const instances: {
 	[id: string]: Accordion | undefined;
@@ -311,19 +312,7 @@ export function accordion(id: string, options: Partial<Options> = {}): Accordion
 			item.header.classList.add(activeHeaderClass);
 		}
 
-		if (!animate || !hasHeightTransition(item.panel)) {
-			item.panel.style.height = '';
-			item.panel.style.display = 'block';
-		} else {
-			let startHeight = item.panel.clientHeight;
-
-			item.panel.style.height = 'auto';
-			item.panel.style.display = 'block';
-
-			let endHeight = item.panel.clientHeight;
-
-			animateHeight(item.panel, startHeight, endHeight);
-		}
+		up(item.panel, animate);
 
 		if (!multiselect) {
 			close(activeIds, options);
@@ -356,11 +345,7 @@ export function accordion(id: string, options: Partial<Options> = {}): Accordion
 			item.header.classList.remove(activeHeaderClass);
 		}
 
-		if (!animate || !hasHeightTransition(item.panel)) {
-			item.panel.style.display = 'none';
-		} else {
-			animateHeight(item.panel, item.panel.clientHeight, 0);
-		}
+		down(item.panel, animate);
 
 		let index = activeIds.indexOf(item.id);
 
@@ -392,61 +377,6 @@ export function accordion(id: string, options: Partial<Options> = {}): Accordion
 
 		keys[key]();
 		event.preventDefault();
-	}
-
-	/**
-	 * Test if an Element has a height transition with a non zero duration.
-	 *
-	 * A panel may not have a CSS height transition set or its duration may be
-	 * zero. In these scenarios either the transitionEnd event is not fired,
-	 * event.propertyName is never equal to ‘height’, or the event may be fired
-	 * before we can catch it. We therefore need to check that the panel
-	 * will animate.
-	 */
-	function hasHeightTransition(element: Element) {
-		let style = window.getComputedStyle(element);
-		let property = style.getPropertyValue('transition-property');
-		let duration = parseFloat(style.getPropertyValue('transition-duration'));
-
-		return duration > 0 && ['all', 'height'].includes(property);
-	}
-
-	/**
-	 * Animates the height of an element between two values.
-	 *
-	 * @param element - The element to animate.
-	 * @param start - The start height of the element.
-	 * @param end - The end height of the element.
-	 */
-	function animateHeight(element: HTMLElement, start: number, end: number) {
-		window.requestAnimationFrame(() => {
-			element.style.height = `${ start }px`;
-
-			window.requestAnimationFrame(() => {
-				element.style.height = `${ end }px`;
-				element.addEventListener('transitionend', onTransitionend);
-			});
-		});
-	}
-
-	function onTransitionend(event: TransitionEvent) {
-		if (event.propertyName !== 'height') {
-			return;
-		}
-
-		let element = event.currentTarget;
-
-		if (!(element instanceof HTMLElement)) {
-			return;
-		}
-
-		if (getInnerHeight(element) === 0) {
-			element.style.display = 'none';
-		}
-
-		element.style.height = '';
-
-		element.removeEventListener('transitionend', onTransitionend);
 	}
 
 	let on: EventFunctionSignature = function(type, listener) {
--- src/utils.ts
+++ src/utils.ts
@@ -7,19 +7,3 @@
  export function modulo(x: number, n: number) {
 	return ((x % n) + n) % n;
 }
-
-/**
- * Gets the height of an element, including padding.
- *
- * @param element
- * @returns The elements height
- */
-export function getInnerHeight(element: HTMLElement) {
-	let style = window.getComputedStyle(element);
-
-	let height = parseInt(style.getPropertyValue('height'), 10);
-	let paddingTop = parseInt(style.getPropertyValue('padding-top'), 10);
-	let paddingBottom = parseInt(style.getPropertyValue('padding-bottom'), 10);
-
-	return height - paddingTop - paddingBottom;
-}
